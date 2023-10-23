0ec35a1e2981c9a169d9f8488c3fa4401db7d15c
Jyoti Singh <singhjyoti1409@gmail.com>
2023-10-24 01:28:56 +0530

    last comment

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import Clock from "./Clock"
+import Clock from "./Clock";
 
 function UserProfile() {
+  const divsToRepeat = [1, 2, 3];
   const { userId } = useParams();
   const [user, setUser] = useState({});
   const [posts, setPosts] = useState([]);
@@ -31,32 +32,34 @@ function UserProfile() {
       });
   }, [userId]);
 
-
   useEffect(() => {
     if (selectedCountry) {
-        axios
-      .get(`http://worldtimeapi.org/api/timezone/${selectedCountry}`)
-      .then((response) => {
-        if (Array.isArray(response.data)) {
-          const countries = response.data;
-          setClockData(countries);
-        } else {
-          setClockData([]);
-        }
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching current time:", error);
-      });
+      axios
+        .get(`http://worldtimeapi.org/api/timezone/${selectedCountry}`)
+        .then((response) => {
+          if (Array.isArray(response.data)) {
+            const countries = response.data;
+            setClockData(countries);
+          } else {
+            setClockData([]);
+          }
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching current time:", error);
+        });
     }
   }, [selectedCountry]);
 
   return (
-    <div>
-      <h1>{user.name}'s Profile</h1>
+    <div className="container">
+        <h1>{user.name}'s Profile</h1>
+        <div className="container-profile">
+        <div className="link">
       <Link to="/">Back</Link>
-
+      </div>
       
+
       <div className="country-selector">
         <h2>Select a Country</h2>
         <select
@@ -64,18 +67,45 @@ function UserProfile() {
           onChange={(e) => setSelectedCountry(e.target.value)}
         >
           <option value="">Select a Country</option>
-          {clockData.length && clockData.map((country) => (
-            <option key={country} value={country}>
-              {country}
-            </option>
-          ))}
+          {clockData.length &&
+            clockData.map((country) => (
+              <option key={country} value={country}>
+                {country}
+              </option>
+            ))}
         </select>
       </div>
+    <div className="country-clock">
+    {selectedCountry && <Clock selectedCountry={selectedCountry} />}
+    </div>
+        </div>
+      
+      
 
-      {selectedCountry && <Clock selectedCountry={selectedCountry} />}
-
-      <div>
+      <div className="user-details">
+        <div className="user-name">
+        <p>Name {user.name}</p>
+        <span>Username | {user.username}</span>
+        </div>
         
+        <div className="user-address">
+          <p>Address:</p>
+          {/* <p>{user.address.street}</p>
+          <p>{user.address.suite}</p>
+          <p>{user.address.city}</p>
+          <p>{user.address.zipcode}</p>
+          <p>Geo: {user.address.geo ? `${user.address.geo.lat}, ${user.address.geo.lng}` : ''}</p> */}
+          <p>User {user.email}</p>
+          <p>Phone {user.phone}</p>
+        </div>
+      </div>
+      <div className="class-third">
+      {divsToRepeat.map((item) => (
+        <div key={item}>
+          
+          <p>This is div {item}</p>
+        </div>
+      ))}
       </div>
     </div>
   );
